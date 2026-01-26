f8af8ebef3c8035de6860a07fb5feea148cf6027
kylejbrk <github@kyleburke.net>
2026-01-26 13:42:41 -0500

    Refactor NodeTree to accept models, sources, and seeds directly

--- dbtui/main.py
+++ dbtui/main.py
@@ -7,9 +7,11 @@ from textual.widgets import Header, Static, Tree
 
 
 class NodeTree(Tree):
-    def __init__(self, project, **kwargs):
+    def __init__(self, models=None, sources=None, seeds=None, **kwargs):
         super().__init__(**kwargs)
-        self.project = project
+        self.models = models
+        self.sources = sources
+        self.seeds = seeds
         self.show_root = False
         self.guide = 2
 
@@ -17,13 +19,20 @@ class NodeTree(Tree):
         self.populate()
 
     def populate(self):
-        model_tree = self.root.add("Models", expand=True)
-        for model in self.project.list_models():
-            model_tree.add_leaf(model.get("name"), data=model)
+        if self.models:
+            model_tree = self.root.add("Models", expand=True)
+            for model in self.models:
+                model_tree.add_leaf(model.get("name"), data=model)
 
-        source_tree = self.root.add("Sources", expand=True)
-        for source in self.project.list_sources():
-            source_tree.add_leaf(source.get("name"), data=source)
+        if self.sources:
+            source_tree = self.root.add("Sources", expand=True)
+            for source in self.sources:
+                source_tree.add_leaf(source.get("name"), data=source)
+
+        if self.seeds:
+            seed_tree = self.root.add("Seeds", expand=True)
+            for seed in self.seeds:
+                seed_tree.add_leaf(seed.get("name"), data=seed)
 
 
 class DBTUI(App):
@@ -41,11 +50,17 @@ class DBTUI(App):
             yield Container(
                 NodeTree(
                     label="node_tree",
-                    project=self.project,
+                    models=self.project.list_models(),
+                    sources=self.project.list_sources(),
+                    seeds=self.project.list_seeds(),
                     id="node_selector",
                 )
             )
             yield Container(Static("Select a node to view details", id="node_details"))
+
+    def on_mount(self):
+        if self.project.project_name:
+            self.title = self.project.project_name
 
 
 if __name__ == "__main__":
@@ -57,7 +72,7 @@ if __name__ == "__main__":
         help="Path to dbt project directory.",
     )
     parser.add_argument(
-        "--dbt_path", type=str, default=None, help="Path to dbt executable."
+        "--dbt-path", type=str, default=None, help="Path to dbt executable."
     )
     args = parser.parse_args()
 
